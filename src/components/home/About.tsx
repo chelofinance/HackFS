2c7c66764e74df40979cf0a8751097160ecdd523
Andres Contreras <andrescontrerasoviedo740@gmail.com>
2022-07-17 13:11:41 -0400

    added logo

--- src/components/home/About.tsx
+++ src/components/home/About.tsx
@@ -26,38 +26,10 @@ const AboutUs: NextPage = () => {
     AOS.init();
   }, []);
 
-  const about = [
-    {
-      className: "flex justify-start items-end w-full",
-      left: true,
-      dataAos: "fade-right",
-      title: "What is Invoice Factoring?",
-      description:
-        "Invoice factoring is a way for UK based businesses to raise money by selling invoices owed to your business to a third party factoringcompany at a discount. Factoring usually includes your own accounts receivable credit control, this is where the lender chases unpaid invoices up on your behalf. UK factoring companies help release cash from your debtor book. Here is everything you need to know about invoice factoring.",
-    },
-    {
-      className: "flex justify-end items-end w-full",
-      dataAos: "fade-left",
-      left: false,
-      title: "How Does Factoring Work?",
-      description:
-        "For invoice factoring to work there must be a factor, a debtor and an unpaid invoice. The factor is the financial institution that offers or agrees to buy business debt or unpaid invoices. The debtor is the client who owes money to a business in the form of an unpaid invoice. Lastly, the invoice is the document that shows transactions between a business and its clients.",
-    },
-
-    {
-      className: "flex justify-start items-end w-full",
-      left: true,
-      dataAos: "fade-right",
-      title: "What is Invoice Factoring?",
-      description:
-        "Invoice factoring is a way for UK based businesses to raise money by selling invoices owed to your business to a third party factoringcompany at a discount. Factoring usually includes your own accounts receivable credit control, this is where the lender chases unpaid invoices up on your behalf. UK factoring companies help release cash from your debtor book. Here is everything you need to know about invoice factoring.",
-    },
-  ];
-
   return (
     <div className="w-full flex flex-col items-center justify-center relative">
       <div className="w-full h-full absolute bottom-0 bg-gradient-to-br via-black from-blue-900/60 to-black"></div>
-      <div className="w-2/3 flex flex-col items-center justify-center gap-10 py-10">
+      <div className="w-3/4 flex flex-col items-center justify-center gap-10 py-10">
         <div
           data-aos="fade-up"
           data-aos-duration="1000"
@@ -65,23 +37,30 @@ const AboutUs: NextPage = () => {
           data-aos-mirror="true"
           data-aos-once="false"
         >
-          <Card className="w-full bg-transparent text-white rounded-xl p-10">
-            <div className=" gap-10 flex flex-col">
+          <Card className="w-full bg-transparent  text-white rounded-xm py-10 px-24">
+            <div className="gap-16 flex flex-col items-center justify-center">
               <h2
                 className={clsx("text-sky-500 text-center font-bold text-3xl")}
               >
-                About Us
+                Why you should choose us?
               </h2>
-              <p className={clsx("text-white text-center font-bold text-lg")}>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. In
-                sapiente aut similique iusto magnam facere pariatur, corporis
-                animi ut enim impedit error placeat accusamus nostrum eius
-                possimus. Inventore, nobis quis? Lorem, ipsum dolor sit amet
-                consectetur adipisicing elit. Eaque nesciunt molestias minima
-                commodi voluptas! Dolore culpa rerum recusandae ex. Porro
-                consequuntur nesciunt aliquid cumque ducimus repudiandae labore
-                voluptatum, quis beatae.
+              <p className={clsx("text-white text-center font-bold text-xl")}>
+                We are a decentralized platform that offers an easy and user
+                friendly process to create invoices powered by Web3 NFT
+                Technology. <br />
+                <br />
+                Those invoices can be sold in an open market, increasing your
+                chances to get fast capital.
+                <br />
+                <br />
+                If the invoice price is too big for only 1 payment, dont worry
+                about it, with DAOFactoring you'll be able to create
+                fractionalized invoices in order to facilitate the purchase of
+                invoices by institutional and retail investors.
               </p>
+              <div className="rounded-full border border-white p-10">
+                <img src="/logo.png" className="h-52" alt="" />
+              </div>
             </div>
           </Card>
         </div>
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -5,7 +5,11 @@ const Navbar = () => {
     <nav className="flex items-center justify-between flex-wrap  p-1  p-5 absolute z-20 w-full shadow-md">
       <div className="flex items-center flex-shrink-0 text-white mr-6">
         <span className="font-semibold text-xl text-white tracking-tight ml-5">
-          <Link href="/">DAOFactoring</Link>
+          <Link href="/">
+            <div className="flex items-center justify-center gap-2">
+              <img src="/logo.png" className="h-6" alt="" /> DAOFactoring
+            </div>
+          </Link>
         </span>
       </div>
       <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto text-white">
